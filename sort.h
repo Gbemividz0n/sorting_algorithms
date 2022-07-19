6ea3a987460c3c72feb6469d98da57794531e0cb
Gbemividz0n <43783192+Gbemividz0n@users.noreply.github.com>
2022-07-19 01:45:35 +0100

    sort_

--- sort.h
+++ sort.h
@@ -1,9 +1,7 @@
-#ifndef _SORT_H_
-#define _SORT_H_
+#ifndef _SORT_
+#define _SORT_
 
 #include <stdlib.h>
-#include <stdio.h>
-#define NUM 10
 
 /**
  * struct listint_s - Doubly linked list node
@@ -19,27 +17,19 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-
-void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-size_t partition(int *array, size_t start, size_t pivot, size_t size);
-void sort(int *array, size_t start, size_t end, size_t size);
-void swap(int *a, int *b);
-void merge_sort(int *array, size_t size);
-void merge_rec(int *array, int *tmp, size_t left, size_t right);
-void print(int *array, char *order, size_t left, size_t right);
-void heap_sort(int *array, size_t size);
-void heap(int *array, size_t size, size_t last);
-void child(int *array, size_t size, size_t last, size_t max);
-void max_heap(int *array, size_t size);
-void swap_heap(int *array, size_t a, size_t b);
-void counting_sort(int *array, size_t size);
-void radix_sort(int *array, size_t size);
-void csort(int *array, size_t size, int div);
+void _qsort(int *a, int low, int high, int size);
 void shell_sort(int *array, size_t size);
-
+void _ssort(int *a, int size, int n);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
 #endif
